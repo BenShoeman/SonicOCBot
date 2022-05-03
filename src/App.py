834937e426b644ec3ce5f5e3ec81d30ee2204078
Ben Shoeman <ben.shoeman@outlook.com>
2022-05-02 21:55:29 -0600

    black formatting

--- src/App.py
+++ src/App.py
@@ -31,9 +31,7 @@ post_probabilities = {
 
 
 def make_post(post_probabilities: dict[str, float] = post_probabilities) -> None:
-    post_probs = [
-        (post, pr) for post, pr in post_probabilities.items()
-    ]
+    post_probs = [(post, pr) for post, pr in post_probabilities.items()]
     posts = [post for post, pr in post_probs]
     probs = [pr for post, pr in post_probs]
     post_typ = random.choices(posts, probs, k=1)[0]
