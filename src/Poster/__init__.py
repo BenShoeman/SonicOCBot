ac7aefed06ada7c19d54d78b4d911f78fb52de6e
Ben Shoeman <ben.shoeman@outlook.com>
2023-02-20 20:00:06 -0700

    some doc fixes

--- src/Poster/__init__.py
+++ src/Poster/__init__.py
@@ -5,6 +5,7 @@ The submodules are as follows:
 
 - **src.Poster.Poster**: Has the abstract Poster class that represents a poster.
 - **src.Poster.DummyPoster**: Has the Poster class that fakes creating a post by showing the PostCreator contents.
+- **src.Poster.FacebookPoster**: Has the Poster class that posts onto Facebook.
 - **src.Poster.MastodonPoster**: Has the Poster class that posts onto Mastodon.
 - **src.Poster.TwitterPoster**: Has the Poster class that posts onto Twitter.
 - **src.Poster.TumblrPoster**: Has the Poster class that posts onto Tumblr.
